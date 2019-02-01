1f7d8482387b3cb5b9fd808a3d9203b7c72efbc8
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2019-02-01 18:01:02 +0530

    save a prettified copy of `make` output to `t.js`

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 default:
 	cd t && pulp build -- --codegen corefn,js
-	cargo run -- --input t --entry Main.main
+	cargo run -- --input t --entry Main.main | prettier --parser babylon /dev/stdin | tee t.js
---
+++ t.js
@@ -0,0 +1,15 @@
+var Main_array = [1, 2, 3];
+var Main_boolean = true;
+var Main_char = "π";
+var Main_int = 42;
+var Main_number = 1.23;
+var Main_string = "πr²";
+var Main_object = {
+  array: Main_array,
+  boolean: Main_boolean,
+  char: Main_char,
+  int: Main_int,
+  number: Main_number,
+  string: Main_string
+};
+var Main_main = Main_object;
